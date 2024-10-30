1923ced04d6a9b02de4808c2834a541f59513015
KevinPXu <kevinpxu21@gmail.com>
2024-10-30 14:53:42 -0400

    test

--- BankHeist-evn.py
+++ BankHeist-evn.py
@@ -6,13 +6,14 @@ import numpy as np
 ale = ALEInterface()
 
 gym.register_envs(ale_py)
-env = gym.make('BankHeist-v3', render_mode="human")
+env = gym.make('BankHeist-v4', render_mode="human")
 obs, info = env.reset()
-for _ in range(99):
+for _ in range(100):
     action = env.action_space.sample()
     obs, reward, done, truncated, info = env.step(action)
 
     if done or truncated:
         obs, info = env.reset()
+print(env.action_space)
 
 env.close()
