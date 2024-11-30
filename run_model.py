b3675eba700ae5a3810a11a4ef2b0a12398e1944
frogperson42 <crogerson42@gmail.com>
2024-11-30 14:50:06 -0500

    Additional features to improve learning

--- run_model.py
+++ run_model.py
@@ -8,6 +8,7 @@ from Preprocess_env import AtariPreprocessing
 from cnn import cnn as CNN
 import matplotlib.pyplot as plt
 import time
+import random
 
 actions_dict = {
     0: "NOOP",
@@ -32,11 +33,11 @@ actions_dict = {
 
 BATCH_SIZE = 32
 
-def run_agent(training_file, gui=False, ):
+def run_agent(training_file, gui=False, random_start=False):
     '''
     Initialize a gym environment of the "Bank Heist" Atari game
     Load CNN parameters from a previous training session
-    Display gameplay by the CNN-powered agent
+    Display gameplay by the CNN-powered agent (if GUI is enabled)
     '''
     # Create game environment
     env = gym.make('BankHeist-v4', frameskip=1)
@@ -68,19 +69,48 @@ def run_agent(training_file, gui=False, ):
     state, _ = env.reset()
     total_reward = 0
 
+    moves=0
+
+    frame_skip = 1
+    frame_counter = 0 
+
     # Gameplay loop
     while not done:
-        with torch.no_grad():
-            state_tensor = torch.from_numpy(state).unsqueeze(0).float().to(device)
-            state_tensor = state_tensor.reshape(state_tensor.size(0), -1, 84, 84).to(device)
+        if frame_counter % frame_skip == 0:
+            if moves < 100 and random_start:
+                action_choices = None
+                # action_choices = torch.tensor([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
 
-            action_choices = policy_nn(state_tensor)
-            action = action_choices.argmax().item()
-        state, reward, done, truncated, _ = env.step(action)
-        done = done or truncated
-        #env.render()  # consider moving everything below to a helper function
+                if moves < 10:
+                    action = 3
+                    state, reward, done, truncated, _ = env.step(3) # right
 
-        total_reward += reward
+                elif moves < 20:
+                    if random.random() > 0.5:
+                        action = 2
+                        state, reward, done, truncated, _ = env.step(2) # up
+                    else:
+                        action = 5
+                        state, reward, done, truncated, _= env.step(5) # down
+                
+                else:
+                    action = random.choice([0,2,3,5])
+                    state, reward, done, truncated, _ = env.step(action)
+                
+                moves += 1
+
+            else:
+                with torch.no_grad():
+                    state_tensor = torch.from_numpy(state).unsqueeze(0).float().to(device)
+                    state_tensor = state_tensor.reshape(state_tensor.size(0), -1, 84, 84).to(device)
+
+                    action_choices = policy_nn(state_tensor)
+                    action = action_choices.argmax().item()
+                state, reward, done, truncated, _ = env.step(action)
+                done = done or truncated
+                #env.render()  # consider moving everything below to a helper function
+
+            total_reward += reward
 
         if gui:
             # Clear the current figure
@@ -102,33 +132,58 @@ def run_agent(training_file, gui=False, ):
 
             # Plot Q-values
             plt.subplot(1, 2, 2)
-            q_values = action_choices[0].cpu().numpy()
-            plt.bar(range(env.action_space.n), q_values)
-            plt.bar(range(len(q_values)), q_values, tick_label=list(actions_dict.values()))
-            plt.xticks(rotation=90)
-            plt.xlabel("Actions")
-            plt.ylabel("Q-values")
-            plt.title('Q-values for each action')
+            if action_choices is not None:
+                q_values = action_choices[0].cpu().numpy()
+                plt.bar(range(env.action_space.n), q_values)
+                plt.bar(range(len(q_values)), q_values, tick_label=list(actions_dict.values()))
+                plt.xticks(rotation=90)
+                plt.xlabel("Actions")
+                plt.ylabel("Q-values")
+                plt.title('Q-values for each action')
 
             # Update the display
             plt.draw()
             plt.pause(0.1)
 
+        frame_counter += 1
+
     env.close()
     return total_reward
 
-def main():
+def run_episodes(run_count, gui, filename, random_start=False):
+    '''
+    Run one or more episodes of a trained agent in the environment
+    Load trained agent from filename
+    Can run with/without GUI for display or speed, respectively
+    Can run 1 or more episodes, and collect reward and runtime on each episode
+    '''
+    # remember episode outcomes
     results = []
-    for i in range(20):
-        start_time = time.perf_counter()
-        reward = run_agent("target_nn_BS=32 ES=1.0 ED=0.9995 EM=0.01 G=0.99 TUF=20 ME=2000 LR=0.0001 SEED=42.pth", gui=False)
+
+    # run multiple episodes
+    for i in range(run_count):
+        start_time = time.perf_counter() # track episode runtime
+        reward = run_agent(filename, gui=gui, random_start=random_start) # run episode
+
+        # calculate runtime
         end_time = time.perf_counter()
         runtime = end_time - start_time
+
+        # record results
         results.append({"reward": reward, "runtime": runtime})
         print(f"Reward: {reward} | Runtime: {runtime}")
     
     print("Finished")
     print(results)
+
+def main():
+    # filename = "./nn_weights/target_nn_BS=32 ES=1.0 ED=0.9995 EM=0.01 G=0.99 TUF=20 ME=2000 LR=0.0001 SEED=42.pth" # well-trained; exploit
+    # filename = "./nn_weights/policy_nn_BS=32 ES=1.0 ED=0.9995 EM=0.01 G=0.99 TUF=20 ME=800 LR=0.0001 SEED=42 RS=True.pth" # random start try #1 (didn't work)
+    # filename = "./nn_weights/target_nn_BS=32 ES=1.0 ED=0.9999 EM=0.01 G=0.99 TUF=20 ME=1000 LR=0.0001 SEED=42 RS=True.pth" # random start try #2 (disallow inf. repetition)
+    # filename = "./nn_weights/policy_nn_BS=32 ES=1.0 ED=0.9995 EM=0.01 G=0.99 TUF=20 ME=200 LR=0.0001 SEED=42 RS=True.pth" # all the bells and whistles (frame skip, actually fixed repetition)
+    filename = "./nn_weights/policy_nn_BS=32 ES=1.0 ED=0.997 EM=0.01 G=0.99 TUF=20 ME=1000 LR=0.0001 SEED=42 RS=True.pth"
+    run_episodes(20, False, filename, random_start=False) # collect data on trials
+    # run_episodes(1, True, filename, random_start=False)   # visualize actions
 
 if __name__ == "__main__":
     main()
--- train_cnn.py
+++ train_cnn.py
@@ -11,20 +11,23 @@ import matplotlib.pyplot as plt
 import time
 # from replay_buffer import ReplayBuffer
 import torch.nn as nn
+from collections import deque
 
 BATCH_SIZE = 32 #32
 
 EPSILON_START = 1.0     #1.0
-EPSILON_DECAY = 0.9995  #0.9995
+EPSILON_DECAY = 0.997  #0.9995
 MIN_EPSILON = 0.01      #0.01
 GAMMA = 0.99            #0.99
 TARGET_UPDATE_FREQ = 20 #20
-MAX_EPISODES = 2000     #2000
+MAX_EPISODES = 1000     #2000
 LEARNING_RATE = 0.0001  #0.0001
 
 SEED = 42 #42
 
-param_str = f"BS={BATCH_SIZE} ES={EPSILON_START} ED={EPSILON_DECAY} EM={MIN_EPSILON} G={GAMMA} TUF={TARGET_UPDATE_FREQ} ME={MAX_EPISODES} LR={LEARNING_RATE} SEED={SEED}"
+RANDOMIZE_START = True
+
+param_str = f"BS={BATCH_SIZE} ES={EPSILON_START} ED={EPSILON_DECAY} EM={MIN_EPSILON} G={GAMMA} TUF={TARGET_UPDATE_FREQ} ME={MAX_EPISODES} LR={LEARNING_RATE} SEED={SEED} RS={RANDOMIZE_START}"
 
 from collections import deque
 class ReplayBuffer:
@@ -132,13 +135,14 @@ def main():
     replay_buffer = ReplayBuffer(10000)
 
     epsilon = EPSILON_START
+    max_reward = 0
 
     # Train over a defined number of gameplay episodes
     for episode in range(MAX_EPISODES):
         # Print out a progress update every 10 episodes
         if episode % 10 == 0:
             # pass # so as to not print every episode
-            print(f"Episode {episode}/{MAX_EPISODES}")
+            print(f"Episode {episode}/{MAX_EPISODES} --- Epsilon={epsilon:.2f} --- Max Reward: {max_reward}")
 
         state, _ = env.reset(seed=SEED)
 
@@ -146,20 +150,58 @@ def main():
 
         total_reward = 0
         done = False
+        recent_moves = deque(maxlen=10)
+
+        if RANDOMIZE_START:
+            moves = 0
+            choice_up_down = random.random() > 0.5
+
+            # randomize start location
+            while moves < 100:
+
+                if moves < 10:
+                    state, reward, done, truncated, _ = env.step(3) # right
+
+                elif moves < 20:
+                    if choice_up_down:
+                        state, reward, done, truncated, _ = env.step(2) # up
+                    else:
+                        state, reward, done, truncated, _= env.step(5) # down
+                
+                else:
+                    action = random.choice([0,2,3,5]) # allow no-op; do NOT allow movement back to left
+                    state, reward, done, truncated, _ = env.step(action)
+                
+                moves += 1
+
+        frame_skip = 1
+        frame_counter = 0
 
         total_steps = 0
         while not done:
-            # Epsilon-greedy choice of action
-            if random.random() < epsilon:
-                # action = env.action_space.sample()
-                action = random.choice([0,2,3,4,5]) # only allow movement
-                # 'no-op' and 'fire' lead to poor training - perhaps after initial training, fire could be introduced
-            else:
-                with torch.no_grad():
-                    # state_tensor = torch.tensor([state], dtype=torch.float32).to(device) # causes warning
-                    state_tensor = torch.from_numpy(state).unsqueeze(0).float().to(device) # faster and warning-free
-                    state_tensor = state_tensor.reshape(1, -1, 84, 84)
-                    action = policy_nn(state_tensor).argmax().item()
+            if frame_counter % frame_skip == 0:
+                if len(recent_moves) < 10 or len(set(recent_moves)) > 1:
+                    # Epsilon-greedy choice of action
+                    if random.random() < epsilon:
+                        # action = env.action_space.sample()
+                        # action = random.choice([0,2,3,4,5])
+                        action = random.choice([0,2,3,4,5,6,7,8,9]) # only allow movement
+                        # 'no-op' and 'fire' lead to poor training - perhaps after initial training, fire could be introduced
+                    else:
+                        with torch.no_grad():
+                            # state_tensor = torch.tensor([state], dtype=torch.float32).to(device) # causes warning
+                            state_tensor = torch.from_numpy(state).unsqueeze(0).float().to(device) # faster and warning-free
+                            state_tensor = state_tensor.reshape(1, -1, 84, 84)
+                            action = policy_nn(state_tensor).argmax().item()
+                else:
+                    # If the last 10 moves are the same, choose a different action
+                    valid_actions = [0, 2, 3, 4, 5]
+                    if recent_moves[-1] in valid_actions:
+                        last_move = recent_moves[-1]
+                        valid_actions.remove(last_move)  # Remove the last move to prevent repeating
+                    action = random.choice(valid_actions)  # Pick a different valid action
+
+                recent_moves.append(action)
 
             # Take the chosen action in the environment; update state accordingly
             next_state, reward, done, truncated, _ = env.step(action)
@@ -174,18 +216,26 @@ def main():
             if total_steps > 0 and total_steps % BATCH_SIZE == 0:
                 train(policy_nn, target_nn, replay_buffer, optimizer, BATCH_SIZE, GAMMA, device)
             total_steps += 1
+            frame_counter += 1
 
         # Epsilon decacys over time
         epsilon = max(MIN_EPSILON, epsilon * EPSILON_DECAY)
+
+        if total_reward > max_reward:
+            max_reward = total_reward
 
         # Update the target network less frequently than the policy network
         if episode % TARGET_UPDATE_FREQ == 0:
             target_nn.load_state_dict(policy_nn.state_dict())
 
     # After training episodes are complete, save the trained CNNs
-    torch.save(policy_nn.state_dict(), f"policy_nn_{param_str}.pth")
-    torch.save(target_nn.state_dict(), f"target_nn_{param_str}.pth")
-    print("Model saved as policy_nn.pth and target_nn.pth")
+    folder = "nn_weights"
+
+    torch.save(policy_nn.state_dict(), f"./{folder}/policy_nn_{param_str}.pth")
+    torch.save(target_nn.state_dict(), f"./{folder}/target_nn_{param_str}.pth")
+    print(f"Model saved as:\n"
+          f"./{folder}/policy_nn_{param_str}.pth\n"
+          f"./{folder}/target_nn_{param_str}.pth")
 
     env.close()
 
